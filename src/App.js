faee9e04b30bf09e55e1c434bea3504d98bb3ace
lmjing <lmjing4905@gmail.com>
2021-12-06 23:33:55 +0900

    코딩 애플 1.2 state 학습 응용 프로젝트 리스트 UI 개발

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import logo from './logo.svg'
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-      <div className="App">
-        <header className="black-nav">
-          <img src={logo} alt="logo" style={{width: '30px'}}/>
-          <div>React Portfolio</div>
-        </header>
-      </div>
-  );
+
+    const [projects, setProjects] = useState([{
+        name: 'project1',
+        date: '2021.12.06'
+    }, {
+        name: 'project2',
+        date: '2022.01.06'
+    }])
+
+    return (
+        <div className="App">
+            <header className="black-nav">
+                <img src={logo} alt="logo" style={{width: '30px'}}/>
+                <div>React Portfolio</div>
+            </header>
+            <div className="main-container">
+                {
+                    projects.map(project => {
+                        const {name, date} = project
+                        return (
+                            <div className="project">
+                                <h3>{name}</h3>
+                                <p>{date}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
