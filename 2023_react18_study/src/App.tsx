89e60f40a7490ff7c9e928b49addd8e7d69a1f91
lmjing <lmjing1234@gmail.com>
2023-02-12 14:20:51 +0900

    mission. 4.11.new tag

--- 2023_react18_study/src/App.tsx
+++ 2023_react18_study/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <Profile
         name={'James Kim'}
         job={'FE 개발자'}
+        isNew
         imageUrl={
           'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
         }
--- 2023_react18_study/src/basic/Profile.tsx
+++ 2023_react18_study/src/basic/Profile.tsx
@@ -4,12 +4,14 @@ interface ProfileProps {
   imageUrl: string;
   name: string;
   job: string;
+  isNew: boolean;
 }
 
-export default function Profile({ imageUrl, name, job }: ProfileProps) {
+export default function Profile({ imageUrl, name, job, isNew }: ProfileProps) {
   return (
     <div className="profile">
       <img className="photo" src={imageUrl} alt="profile" />
+      {isNew && <span className="new">New</span>}
       <h1>{name}</h1>
       <p>{job}</p>
     </div>
